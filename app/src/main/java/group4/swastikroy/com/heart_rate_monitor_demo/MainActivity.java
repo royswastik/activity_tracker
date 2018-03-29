7af0a51aabc6475aff8187ae0f6326b7782d490d
royswastik <swastikroy1993@gmail.com>
2018-03-29 15:55:40 -0700

    Initial branch commit

---
+++ app/src/main/java/group4/swastikroy/com/heart_rate_monitor_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package group4.swastikroy.com.heart_rate_monitor_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/group4/swastikroy/com/heart_rate_monitor_demo/ui/ClassificationActivity.java
@@ -0,0 +1,15 @@
+package group4.swastikroy.com.heart_rate_monitor_demo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import group4.swastikroy.com.heart_rate_monitor_demo.R;
+
+public class ClassificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classification);
+    }
+}
