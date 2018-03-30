cd34f143746239c919b0ab64bfe4c8d022afe0e4
royswastik <swastikroy1993@gmail.com>
2018-03-29 19:00:52 -0700

    Add Performance Visualization Activity

---
+++ app/src/main/java/group4/swastikroy/com/heart_rate_monitor_demo/model/PowerConsumption.java
@@ -0,0 +1,26 @@
+package group4.swastikroy.com.heart_rate_monitor_demo.model;
+
+/**
+ * Created by sroy41 on 3/29/2018.
+ */
+
+public class PowerConsumption {
+    double timeInstance;
+    double powerConsumption;
+
+    public double getTimeInstance() {
+        return timeInstance;
+    }
+
+    public void setTimeInstance(double timeInstance) {
+        this.timeInstance = timeInstance;
+    }
+
+    public double getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public void setPowerConsumption(double powerConsumption) {
+        this.powerConsumption = powerConsumption;
+    }
+}
